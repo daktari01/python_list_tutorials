a76fde324f373a856878bd631a8b5e616fd3696b
Daktari <dindijjames@gmail.com>
2017-10-11 20:02:27 +0000

    Worked on Dictionary, Set and Generator Comprehensions

--- dictionaries_compr.py
+++ dictionaries_compr.py
@@ -1,4 +1,45 @@
 #Dictionary comprehensions
 names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
-heroes = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
-print (zip(names, heroes))
+heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
+#print (zip(names, heros))
+
+#I want a dict{'name': 'hero'} for each name, hero in zip(name, heros)
+my_dict = {}
+for name, hero in zip(names, heros):
+    my_dict[name] = hero
+print(my_dict)
+
+#I want a dict{'name': 'hero'} for each name, hero in zip(name, heros)
+#As a dictionary comprehension
+my_dict = {name: hero for name, hero in zip(names, heros)}
+print(my_dict)
+
+#If name is not equal to Peter
+my_dict = {name: hero for name, hero in zip(names, heros) if name != 'Peter'}
+print(my_dict)
+
+#Set Comprehensions
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nums = [1, 1, 2, 1, 3, 4, 3, 4, 5, 5, 6, 7, 8, 7, 9, 9]
+my_set = set()
+for n in nums:
+    my_set.add(n)
+print(my_set)
+
+#Using set comprehensions
+my_set = {n for n in nums}
+print(my_set)
+
+#Generator Expressions
+#I want to yield 'n * n' for 'n' in nums
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+'''def get_func(nums):
+    for n in nums:
+        yield n * n
+        '''
+#Using generator comprehensions
+my_gen = (n*n for n in nums)
+
+#my_gen = get_func(nums)
+for i in my_gen:
+    print(i)
